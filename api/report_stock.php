c50494d380eed2789c215a2cd7a482115bd264ed
waitayafolk <62681214+waitayafolk@users.noreply.github.com>
2021-02-25 11:33:53 +0700

    Update report_stock.php

--- api/report_stock.php
+++ api/report_stock.php
@@ -9,7 +9,7 @@ $params = json_decode(file_get_contents('php://input'));
     LEFT JOIN tb_bill_sale_detail ON tb_bill_sale_detail.product_id = tb_product.id
     LEFT JOIN tb_stock_detail ON tb_stock_detail.product_id = tb_product.id
     WHERE tb_product.status = 'use' 
-    GROUP BY tb_product.id 
+    GROUP BY tb_bill_sale_detail.qty , tb_stock_detail.qty
     ORDER BY tb_product.id DESC";
 
     $stmt = $pdo->prepare($sql);
